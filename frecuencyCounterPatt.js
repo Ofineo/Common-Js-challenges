83b89124e21e8e04c00de78891e9149d33abedfb
Jordi <ofineo@gmail.com>
2020-05-29 16:19:14 +0100

    frequency counter pattern

---
+++ frecuencyCounterPatt.js
@@ -0,0 +1,27 @@
+
+function validAnagram(str1,str2){
+    if (str1.length != str2.length) return false
+    let freqCount1={}
+    let freqCount2={}
+
+    for(let val of str1){
+        freqCount1[val]= (freqCount1[val] || 0)+1
+    }
+    for(let val of str2){
+        freqCount2[val]= (freqCount2[val] || 0)+1
+    }
+    for(let key in freqCount1){
+        if(!(key in freqCount2)){
+            return false
+        }
+        if(freqCount2[key] !== freqCount1[key]){
+            return false
+        }
+    }
+    return true
+   
+    return 
+}
+
+console.log(validAnagram('anagram','nagaram'))
+console.log(validAnagram('anagram','fnagaram'))
