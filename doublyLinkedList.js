c1b2f94b8f4f1c9fa7947a24ea41b430c1209c4e
Jordi <ofineo@gmail.com>
2020-06-01 14:39:48 +0100

    Doubly Linked List unshift

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -1,16 +1,90 @@
-class Node{
-    constructor(val){
-        this.val = val;
-        this.next= null;
-        this.prev = null;
-    }
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
 }
 
-class DoublyLinkedList{
-    constructor() {
-        this.length = 0;
-        this.tail = null;
-        this.head = null;
-      }
+class DoublyLinkedList {
+  constructor() {
+    this.length = 0;
+    this.tail = null;
+    this.head = null;
+  }
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.head) return undefined;
 
+    let removedNode = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = removedNode.prev;
+      removedNode.prev = null;
+      this.tail.next = null;
+    }
+    this.length--;
+    return removedNode;
+  }
+  shift() {
+    if (!this.head) return undefined;
+    let removedNode = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = removedNode.next;
+      this.head.prev = null;
+      removedNode.next = null;
+    }
+    this.length--;
+    return removedNode;
+  }
+  unshift(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    let count, currentNode;
+    if (index <= this.length / 2) {
+      count = 0;
+      currentNode = this.head;
+      while (count !== index) {
+        currentNode = currentNode.next;
+        count++;
+      }
+    } else {
+      count = this.length - 1;
+      currentNode = this.tail;
+      while (count !== index) {
+        currentNode = currentNode.prev;
+        count--;
+      }
+    }
+    return currentNode;
+  }
 }
