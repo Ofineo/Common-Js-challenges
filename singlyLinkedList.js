843e8e575ba25ac5dcdfcb4a469bc373a99eb597
Jordi <ofineo@gmail.com>
2020-06-01 09:18:17 +0100

    added method unshift

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -38,12 +38,49 @@ class SinglyLinkedList {
       node.next;
     }
   }
+  shift() {
+    if (!this.head) return undefined;
+
+    let currentHead = this.head;
+    this.head = currentHead.next;
+    currentHead.next = null;
+    this.length--;
+    if (this.length === 0) this.tail = null;
+    return currentHead;
+  }
+  unshift(val) {
+    if (!this.head) {
+      this.push(val);
+    } else {
+      let newNode = new node(val);
+      newNode.next = this.head;
+      this.head = newNode;
+      this.length++;
+    }
+    
+    return this;
+  }
 }
 
-let a = new SinglyLinkedList;
+//SinglyLinkedList testing
+let a = new SinglyLinkedList();
 a.push(1);
 a.push(12);
 console.log(a.head.val, a.head.next.val);
 
 console.log(a.pop());
 console.log(a);
+//shifting test
+console.assert(a.length == 1);
+a.push(13);
+a.push(23);
+console.assert(a.length == 3);
+a.shift();
+console.assert(a.head.val == 13);
+console.assert(a.tail.val == 23);
+console.log(a.shift());
+console.assert(a.length == 1);
+
+console.log(a);
+console.assert(a.head.val == 23);
+console.assert(a.tail.val == 23);
