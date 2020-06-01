22a37eccef427a8e9188f4a4191df792c7967df3
Jordi <ofineo@gmail.com>
2020-06-01 19:41:21 +0100

    data types queue build

---
+++ data types/queue.js
@@ -0,0 +1,34 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  enqueue(val) {
+    let newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    return ++this.size;
+  }
+  dequeue() {
+    if (!this.first) return null;
+    let delNode = this.first;
+    this.first = delNode.next;
+    delNode.next = null;
+    if (this.size === 0) this.last = null;
+    this.size--;
+    return delNode;
+  }
+}
