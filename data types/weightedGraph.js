e86c9dcc1d0e735ccbae94e3f6f7f6100226640a
Jordi <ofineo@gmail.com>
2020-06-06 16:01:54 +0100

    alogarithm dijsktras

--- data types/weightedGraph.js
+++ data types/weightedGraph.js
@@ -1,3 +1,5 @@
+import PriorityQueue from './naivePriorityQueue';
+
 class Edge {
   constructor(node, weight) {
     this.node = node;
@@ -16,9 +18,37 @@ class WeightedGraph {
       this.adjacencyList[vertex1].push(new Edge(vertex1,weight));
       this.adjacencyList[vertex2].push(new Edge(vertex2,weight));
   }
+  find(startVertex, endingVertex){
+      let distances = {};
+      let queue = new PriorityQueue();
+      let previous = {};
+
+      for(const vertex in this.adjacencyList){
+          if(vertex === startVertex) continue;
+          distances[vertex] = Infinity;
+          previous[vertex] = null;
+          queue.enqueue(vertex,Infinity);
+          let current;
+
+          while(queue.length){
+            current = queue.dequeue();
+            if (current===endingVertex) return distances;
+            this.adjacencyList[current].forEach(element => {
+                
+            });
+
+          }
+      }
+
+
+  }
 }
 let b = new WeightedGraph();
 b.addVertex('A');
 b.addVertex('B');
 b.addVertex('C');
+b.addVertex('D');
 b.addEdge('A','B',210);
+b.addEdge('A','C',110);
+b.addEdge('C','B',90);
+b.addEdge('C','D',30);
